21061ff0a7d600bad2f8dc4b5265ad5b0a220640
Firealem Erko <firealemg@yahoo.com>
2019-04-09 18:12:55 -0400

    end of MVP

--- js/index.js
+++ js/index.js
@@ -12,12 +12,12 @@ function cbfun1(event){
 
 //mousemove
 
-const containerselector = document.querySelector('html');
+const containerselector = document.querySelector('body');
 
 containerselector.addEventListener("mousemove", cbfun2);
 
 function cbfun2(event){
-document.body.style.backgroundColor = "rgb("+event.offsetX+","+event.offsetX+", 100)";
+document.body.style.backgroundColor = "rgb("+event.offsetX+","+event.offsetX+", 255)";
 }
 
 //double click event , delete every image when clicked
@@ -46,5 +46,56 @@ event.target.style.color='red'
 }
 }
 
+//mouseenter
+
+for ( var i=0; i<picselector.length; i++)
+{
+    picselector[i].addEventListener('mouseenter', cbfun5)
+  function cbfun5 (event){
+      event.target.style.opacity='0.3'
+  }
+}
+
+//mouseleave
+for ( var i=0; i<picselector.length; i++)
+{
+    picselector[i].addEventListener('mouseleave', cbfun6)
+  function cbfun6 (event){
+      event.target.style.opacity='1'
+  }
+}
+
+ //prevent spacebar from scrolling down 
+
+ window.addEventListener('keydown', cbfun7)
+ 
+ function cbfun7(event) { 
+     if(event.keyCode===32)
+     event.preventDefault();
+ }
+
+//resize event listner
+const navcont = document.querySelector('nav')
+navcont.addEventListener("resize", cbfun8)
 
 
+function cbfun8 (event){
+      event.target.style.backgroundColor ='green';
+}
+
+//prevent event propagation
+const introimg = document.querySelector('.intro img')
+introimg.addEventListener("mousemove", cbfun10);
+
+function cbfun10 (event)
+{
+    event.stopPropagation();
+}
+// nav item prevent default
+
+const navitem = document.querySelector('.nav a');
+navitem.addEventListener('click', cbfun9)
+
+function cbfun9 (event){
+    event.preventDefault();
+}
